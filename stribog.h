7e1303797e823b47ea5373e681b2ffc698889ec0
Sergey V <sftp.mtuci@gmail.com>
2015-05-12 19:46:50 +0300

    add licensing

--- stribog.h
+++ stribog.h
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2013 by sftp
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted.
+ *
+ * There's ABSOLUTELY NO WARRANTY, express or implied.
+ */
+
 #ifndef _STRIBOG_H
 #define _STRIBOG_H
 
